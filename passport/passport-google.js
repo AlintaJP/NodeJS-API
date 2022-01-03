4627e54d390ab3c6dc0eb91380e41ee8c18909a0
Yegor <alinta.kawakami+github@gmail.com>
2022-01-03 11:09:49 +0300

    Update passport-google.js

--- passport/passport-google.js
+++ passport/passport-google.js
@@ -5,7 +5,7 @@ const strategy = new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `http://127.0.0.1:3000/auth/google/callback`,
+    callbackURL: `https://mighty-ocean-38818.herokuapp.com/auth/google/callback`,
     passReqToCallback: true,
   },
   function (request, accessToken, refreshToken, profile, done) {
